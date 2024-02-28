0cd1312cd9e4ea99b60844ccc961c5134c33929f
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-02-28 16:31:10 -0300

    app

--- App.jsx
+++ App.jsx
@@ -3,24 +3,47 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from './src/screen/Home';
-import Beard from './src/screen/Beard';
 import Hair from './src/screen/Hair';
 
 export default function App() {
- const Stack = createNativeStackNavigator
+ const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: "Zé Barber",
+            headerStyle: {
+              backgroundColor: "#7c3b1a",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerTitleAlign: "center",
+            
+          }}
+        />
+
+        <Stack.Screen
+          name="Hair"
+          component={Hair}
+          options={{
+            title: "Cabelo",
+            headerStyle: {
+              backgroundColor: "#E0E0E0",
+            },
+            headerTintColor: "#000",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            
+          }}
+        />
+      </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
