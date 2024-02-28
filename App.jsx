dea6ee90cfc10238ee5fe3b64f6773af6cb164a2
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-02-28 15:22:44 -0300

    criando as pastas

--- App.jsx
+++ App.jsx
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from './src/screen/Home';
+import Beard from './src/screen/Beard';
+import Hair from './src/screen/Hair';
 
 export default function App() {
+ const Stack = createNativeStackNavigator
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
